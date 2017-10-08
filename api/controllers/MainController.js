02ee45f4f4502e99d4d816a6c9f9506d217b3225
Bhumikajigar <30588755+Bhumikajigar@users.noreply.github.com>
2017-10-08 13:32:50 +1100

    test

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -1,3 +1,14 @@
+/**
+ * MainController
+ *
+ * @description :: Server-side logic for managing Mains
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	gitDeploy: function (req, res) {
+  if(req.param('pw')=="< yourSecretPassword >") {
+     var sys = require('sys')
      var exec = require('child_process').exec;
      function puts(error, stdout, stderr) {
          sys.puts(stdout) 
